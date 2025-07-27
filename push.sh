7d4d78d6a28bbb8018796e3aabd08158f4753404
Moe-hacker <moe-hacker@outlook.com>
2025-07-28 00:26:25 +0800

    Ok

--- push.sh
+++ push.sh
@@ -8,9 +8,20 @@ cp -v ./_config.yml ${BK_REPO}/
 cp -v ./node_modules/hexo-theme-butterfly/_config.yml  ${BK_REPO}/_config-butterfly.yml
 cp -v ./package*.json ${BK_REPO}/
 cp -rv ./push.sh ${BK_REPO}/
+cp -v ./cmsg.py ${BK_REPO}/
 cd ${BK_REPO}
 git add .
-git commit -m "site update $(date)"
+while true; do
+  CMSG=$(./cmsg.py || echo "up")
+  echo "Commit message: $CMSG"
+  read -p "Do you like this one? (y/n) " yn
+  case $yn in
+  [Yy]*) break ;;
+  [Nn]*) continue ;;
+  *) continue ;;
+  esac
+done
+git commit -m "$CMSG"
 git push
 cd ${BLOG_DIR}
 ./node_modules/hexo/bin/hexo g
